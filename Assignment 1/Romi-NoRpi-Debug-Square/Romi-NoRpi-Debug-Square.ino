8e0101f46fd31721bade14e276387372f958b129
tuqueroe <71103932+tuqueroe@users.noreply.github.com>
2020-10-10 16:51:42 -0700

    Add files via upload

---
+++ Assignment 1/Romi-NoRpi-Debug-Square/Romi-NoRpi-Debug-Square.ino
@@ -0,0 +1,60 @@
+//Esteban Tuquero
+//CPE 476
+//Assignment 1b
+
+/*Using the code supplied in https://github.com/venki666/CpE476_demos/tree/master/Arduino/Romi-NoRpi-Debug
+ *b) Develop the code to move the Romi Robot in a square/rectangle (you can use your own parameters) 
+ */
+
+#include <Romi32U4.h>
+#include <PololuRPiSlave.h>
+
+Romi32U4Motors motors;
+Romi32U4Encoders encoders;
+Romi32U4ButtonA buttonA;
+
+void setup() {
+  Serial.begin(57600);
+
+
+  ledYellow(false);
+  ledGreen(true);
+  ledRed(false);
+}
+
+float _debug_linear_ms = 0.25;
+float _debug_angle_rs = 0.00;
+void _DEBUG_PID_CONTROL() {
+  static float _linear_ms_change = 0.1;
+  set_twist_target(_debug_linear_ms, _debug_angle_rs);
+}
+
+
+
+void loop() {
+
+  _DEBUG_PID_CONTROL();
+  
+  //set motor speed to move robot forward for 2 seconds
+  motors.setSpeeds(-100,-100);
+  delay(2000);
+
+  //set motor speed to 0 to stop robot for 2 seconds
+  motors.setSpeeds(0,0);
+  delay(2000);
+
+  //wheels rotate to turn the robot
+  motors.setSpeeds(100,0);
+  delay(1250);  
+
+  //stop for 2 seconds before moving again
+  motors.setSpeeds(0,0);
+  delay(2000);
+      
+  if (everyNmillisec(10)) {
+    // ODOMETRY
+    calculateOdom();
+    doPID();
+  }
+
+}
